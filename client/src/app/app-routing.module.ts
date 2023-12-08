f0f27879f0f342a54787f15ab4a629d49452a583
Hemang Patel <hemang9705@gmail.com>
2023-12-08 23:19:42 +0530

    event form without toggle button

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SingupComponent } from './singup/singup.component';
+import { EventformComponent } from './eventform/eventform.component';
 
 const routes: Routes = [
   {path:'login',component:LoginComponent},
-  {path:'signup',component:SingupComponent}
+  {path:'signup',component:SingupComponent},
+  {path:'eventform',component:EventformComponent}
 ];
 
 @NgModule({
---
+++ client/src/app/eventform/eventform.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventform',
+  templateUrl: './eventform.component.html',
+  styleUrl: './eventform.component.css'
+})
+export class EventformComponent {
+  isTeam=true;
+}
