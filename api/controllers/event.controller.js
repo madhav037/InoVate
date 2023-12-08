f0b54e335fa3777f1d3a2a28704f5e077c51d80b
madhav <109799001+madhav037@users.noreply.github.com>
2023-12-08 21:52:18 +0530

    event backend done

--- api/controllers/event.controller.js
+++ api/controllers/event.controller.js
@@ -26,3 +26,59 @@ export const createEvent = async (req,res,next) => {
         next(error)
     }
 }
+
+export const getAllEvents = async (req,res,next) => {
+    try {
+        const events = await Event.find()
+        res.status(200).json(events)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getEvent = async (req,res,next) => {
+    const id = req.params.id
+    try {
+        const event = await Event.findById(id)
+        res.status(200).json(event)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateEvent = async (req,res,next) => {
+    const id = req.params.id
+    const {title, description, startingDate, endingDate, location, image, resgestrationOpen, registrationStartDate, registrationEndDate, allowTeams, limitOfMembers, comments, createdBy } = req.body
+
+    try {
+        const event = await Event.findByIdAndUpdate(id,{
+            title,
+            description,
+            startingDate,
+            endingDate,
+            location,
+            image,
+            resgestrationOpen,
+            registrationStartDate,
+            registrationEndDate,
+            allowTeams,
+            limitOfMembers,
+            comments,
+            createdBy
+        },{new:true})
+        if(!event) return res.status(404).json({message:"Event not found"})
+        res.status(200).json(event)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deleteEvent = async (req,res,next) => {
+    const id = req.params.id
+    try {
+        const event = await Event.findByIdAndDelete(id)
+        res.status(200).json(event)
+    } catch (error) {
+        next(error)
+    }
+}
--- api/routes/event.route.js
+++ api/routes/event.route.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
 import { verifyAdmin } from '../utils/verifyAdmin.js';
-import { createEvent } from '../controllers/event.controller.js';
+import { createEvent, deleteEvent, getAllEvents, getEvent, updateEvent } from '../controllers/event.controller.js';
 const router = express.Router();
 
 router.post("/create-event", verifyToken, verifyAdmin, createEvent)
+router.get("/get-all-events", getAllEvents)
+router.get("/get-event/:id", getEvent)
+router.put("/update-event/:id", verifyToken, verifyAdmin, updateEvent)
+router.delete("/delete-event/:id", verifyToken, verifyAdmin, deleteEvent)
 
 export default router;
