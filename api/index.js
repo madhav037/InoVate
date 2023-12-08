ab76e2179e4e631a6c9d17d45c1b10980c17ba1a
Aayush2425 <122793682+Aayush2425@users.noreply.github.com>
2023-12-08 16:36:55 +0530

    Connection to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,14 +1,28 @@
-import cookieParser from 'cookie-parser';
-import express from 'express';
+import cookieParser from "cookie-parser";
+import express from "express";
 import authRouter from "./routes/auth.route.js";
-
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 const app = express();
 
 app.listen(3000, () => {
-    console.log('app listening on port 3000');
-})
+  console.log("app listening on port 3000");
+});
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
 app.use(cookieParser());
 
-app.use("/api/auth", authRouter)
+mongoose
+  .connect(
+    "mongodb+srv://AayushGandhi:YPajUaTDpleeamPx@userinfo.b92p5dr.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use("/api/auth", authRouter);
