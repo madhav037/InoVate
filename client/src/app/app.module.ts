5c37a6377927fef99fac37a856ca8201d5e61d5a
Hemang Patel <hemang9705@gmail.com>
2023-12-08 19:21:13 +0530

    sign up page ready

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { SingupComponent } from './singup/singup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SingupComponent
   ],
   imports: [
     BrowserModule,
