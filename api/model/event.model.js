1a88a0fa84e387761ee0a46d32c72ef0f529cc86
Aayush2425 <122793682+Aayush2425@users.noreply.github.com>
2023-12-08 16:24:14 +0530

    Event and Team Module Added

--- api/model/event.model.js
+++ api/model/event.model.js
@@ -1,17 +1,32 @@
 import mongoose from "mongoose";
+import Team from "./team.model";
 
 const eventSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
+
     description: { type: String, required: true },
+
     startingDate: { type: Date, required: true },
+
     endingDate: { type: Date, required: true },
+
     location: { type: String, required: true },
+
     totalParticipants: { type: Number, required: true, default: 0 },
-    image: { type: String, required: true },
-    status: { type: String, required: true },
-    participants: { type: Array, default: [] },
+
+    image: [{ type: String, required: true }],
+
+    resgestrationOpen: { type: Boolean, required: true, default: true },
+
+    registrationStartDate: { type: Date, required: true },
+
+    registrationEndDate: { type: Date, required: true },
+
+    teams: [{ ref: Team }],
+
     comments: { type: Array, default: [] },
+
     createdBy: { type: String, required: true },
   },
   { timestamps: true }
@@ -19,4 +34,4 @@ const eventSchema = new mongoose.Schema(
 
 const Event = mongoose.model("Event", eventSchema);
 
-module.exports = Event;
+export default Event;
