a1d2a58395749e15401df9ac92fed7b729246de9
madhav <109799001+madhav037@users.noreply.github.com>
2023-12-08 16:10:05 +0530

    sign in logic done, added default value in user model, added some dependencies

--- api/model/user.model.js
+++ api/model/user.model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    image: { type: String, required: true },
+    image: { type: String, required: true, default:'https://fontawesome.com/v5/icons/user?f=classic&s=solid' },
     role: {
       type: String,
       enum: ["devadmin", "eventadmin", "participant"],
