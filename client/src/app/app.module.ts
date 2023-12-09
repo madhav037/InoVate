8e29d36ac76476b1ef1c468ec628bbfe14bfd70f
Hemang Patel <hemang9705@gmail.com>
2023-12-09 11:09:30 +0530

    commit

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SingupComponent } from './singup/singup.component'; // Import the missing SingupComponent
@@ -9,18 +8,21 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { EventformComponent } from './eventform/eventform.component';
 import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SingupComponent,
-    EventformComponent ,// Add SingupComponent to the declarations array
-    HomeComponent, // Add SingupComponent to the declarations array
+    EventformComponent ,
+    HomeComponent,
+    SidebarComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule
